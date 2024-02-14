9eeba7f1c4a36eed03a41b427ab1c9f9536f7d71
jhonatan <jhonatanianbauer@gmail.com>
2024-02-14 11:34:30 -0300

    final

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function toggleMode() {
     // substituir a imagem
     if(html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src" , "./assets/avatar.png")
+    img.setAttribute("src" , "./assets/avatar.png");
 
       
 } else { 
